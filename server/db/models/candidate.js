3425702ba8ea9aedd658db7208a9ebdab273b100
Zabarchik <zabaraolga2024@gmail.com>
2025-12-17 17:57:43 +0300

    исправление бд

--- server/db/models/candidate.js
+++ server/db/models/candidate.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-    this.belongsToMany(models.Stages, { through: "CandidatesStages", foreignKey: "candidateId"})
+    this.belongsToMany(models.Stage, { through: "CandidatesStages", foreignKey: "candidateId"})
     }
   }
   Candidate.init({
